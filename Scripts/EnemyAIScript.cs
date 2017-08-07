a45f96605c472847b4319b0b12e90be45c4b3acf
John <john_adebiyi@hotmail.de>
2017-08-08 01:10:39 +0200

    Loose interest in chasing the player if far away
    If player outruns enemy, the enemy chases the player for a certain amount of time and then looses interest and goes back patrolling

--- Scripts/EnemyAIScript.cs
+++ Scripts/EnemyAIScript.cs
@@ -14,16 +14,21 @@ using System.Collections;
 public class EnemyAIScript : MonoBehaviour
 {
     public Transform player;//FPSController
-    public Transform eyes;// A sphere with a disabled mesh renderer is used for eyes
+    public Transform eyes;// A sphere with a disabled mesh renderer is used for the eyes(Radar)
     private Animator anim;
     private NavMeshAgent nav;
 
-
     private string state = "chooseA_SpotToWalk";
     private bool alive = true;
+    private bool isOutside = false;
+    private float wait;//when player out runs the enemy, the enemy chases the player for some time and looses interest
+    private float standStill;// when enemy reaches path position, enemy should stay still for a certain amount of time and move after
 
     private float nearAttack_distance;//when enemy gets near the player 
     private float inFrontAttack_distance;//when enemy is in front of player
+    private float outOfSight;//when player out runs the enemy, the enemy looses interest
+
+
 
 
 
@@ -56,17 +61,18 @@ public class EnemyAIScript : MonoBehaviour
                     if (state != "inPlainSight")
                     {
                         state = "chase";
-                        nav.speed = 2.0f;
+                        nav.speed = 3.5f;
+                        wait = 10f;
                     }
                 }
             }
         }
     }
 
-
     // Update is called once per frame
     void Update()
     {
+
         Debug.DrawLine(eyes.position, player.transform.position, Color.red);//draws a line in the scene window to show distance between player and enemy
         if (alive)
         {
@@ -82,8 +88,9 @@ public class EnemyAIScript : MonoBehaviour
                 NavMeshHit navHit;
                 NavMesh.SamplePosition(transform.position + randomPos, out navHit, 20f, NavMesh.AllAreas);//Find a random position to walk to from the starting point in a radius of 20
                 nav.SetDestination(navHit.position);
+                standStill = 1f;
                 state = "chooseAnotherSpot";
-
+                
             }
 
             //chooses another spot to walk to 
@@ -91,17 +98,52 @@ public class EnemyAIScript : MonoBehaviour
             {
                 if (nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending)
                 {
+                    Debug.Log("arrived");
                     anim.SetBool("isWalking", false);
                     anim.SetBool("isIdle", true);
-                    state = "chooseA_SpotToWalk";
+
+                    // when enemy reaches path position, enemy should stay still for a certain amount of time and move after
+                    if (standStill > 0)
+                    {
+                        standStill -= Time.deltaTime;
+                    }else
+                    {
+                        state = "chooseA_SpotToWalk";
+                    }
                 }
+
+
             }
             //chase
             if (state == "chase")
             {
+                
                 anim.SetBool("isWalking", true);
                 anim.SetBool("isIdle", false);
                 nav.destination = player.transform.position;
+
+                //when player out runs the enemy, the enemy looses interest
+                //if player is out of the enemys field for a certain amount of time the enemy goes back patrolling
+                outOfSight = Vector3.Distance(transform.position, player.transform.position);
+                if (outOfSight > 20)
+                {
+                    Debug.Log("Player is out of enemies sight");
+                    if (wait > 0f)
+                    {
+                        wait -= Time.deltaTime;
+                    }else
+                    {
+                        Debug.Log("Enemy has stopped chasing you");
+                        nav.speed = 1.2f;
+                        state = "chooseAnotherSpot";
+                    }
+
+
+                    
+
+                }
+
+
 
                 //if enemy gets near the player start the walk hit animation
                 nearAttack_distance = Vector3.Distance(transform.position, player.transform.position);
@@ -109,7 +151,7 @@ public class EnemyAIScript : MonoBehaviour
                 {
                     anim.SetBool("isWalkHit", true);
                     anim.SetBool("isWalking", false);
-                    if (nearAttack_distance < 3)
+                    if (nearAttack_distance < 3)// if enemy gets in front of the player
                     {
                         state = "inFrontPlayer";
                     }
@@ -119,14 +161,16 @@ public class EnemyAIScript : MonoBehaviour
                     anim.SetBool("isWalking", true);
                     anim.SetBool("isWalkHit", false);
                 }
+
+
             }
-            //when enemy is in front of player start the idle hit animation and damage player
+            //when enemy is in front of player start the idle hit animation and damage the player
             if (state == "inFrontPlayer")
             {
                 inFrontAttack_distance = Vector3.Distance(transform.position, player.transform.position);
                 if (inFrontAttack_distance < 3)
                 {
-                    Debug.Log("damage");
+                   // Debug.Log("damage");
                     anim.SetBool("isWalkHit", false);
                     anim.SetBool("isIdleHit", true);
                 }else
@@ -134,8 +178,9 @@ public class EnemyAIScript : MonoBehaviour
                     anim.SetBool("isWalkHit", true);
                     anim.SetBool("isIdleHit", false);
                     state = "chase";
-
                 }
+
+
             }
             // nav.SetDestination(player.transform.position);
 
