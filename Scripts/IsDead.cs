ed59726c0f54761afe6d5d9c70b12a390aaa7e71
John <john_adebiyi@hotmail.de>
2017-08-26 13:04:51 +0200

    Gameover
    On death, the player has the options to restart or exit the game

---
+++ Scripts/IsDead.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Diagnostics;
+
+public class IsDead : MonoBehaviour {
+
+      public Transform Player;
+      public UnityEngine.UI.Image bar;
+
+    public void RestartGame()
+    {
+        Process.Start(Application.dataPath + "/../tryx86_64.exe");
+        Application.Quit();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
