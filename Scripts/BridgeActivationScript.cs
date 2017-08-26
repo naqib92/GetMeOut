bcb8e94a7cf89a46bd3af395b4c0be7546f3c1fb
John <john_adebiyi@hotmail.de>
2017-08-26 13:22:25 +0200

    bridge appears after the terminal is activated

---
+++ Scripts/BridgeActivationScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeActivationScript : MonoBehaviour {
+
+    public GameObject bridge;// activate the bridge
+    public GameObject bridgeDoor1;// deactivate the invincible bridge door
+    public GameObject bridgeDoor2;// deactivate the invincible bridge door
+    public GameObject bridgeDoor3;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_4;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_5;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_6;// deactivate the invincible bridge door
+    public GameObject bridgeDoor_7;// deactivate the invincible bridge door
+
+
+    public static bool bridgeIsActiv = false;
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (bridgeIsActiv)
+        {
+            bridge.SetActive(true);
+            bridgeDoor1.SetActive(false);
+            bridgeDoor2.SetActive(false);
+            bridgeDoor3.SetActive(false);
+            bridgeDoor_4.SetActive(false);
+            bridgeDoor_5.SetActive(false);
+            bridgeDoor_6.SetActive(false);
+            bridgeDoor_7.SetActive(false);
+        }
+	}
+
+    
+
+
+}
