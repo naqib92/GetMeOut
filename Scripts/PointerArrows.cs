e09d7cd29498f524971e2c6e5fc947d80a4e0cf1
John <john_adebiyi@hotmail.de>
2017-08-26 13:16:30 +0200

    pointer that points to enemy or to objects
    pointers are placed on the down side of the screen

---
+++ Scripts/PointerArrows.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointerArrows : MonoBehaviour {
+
+
+
+    public Transform target;
+   // public float speed;
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - target.transform.position);
+
+        /**
+        Vector3 targetDir = target.position - transform.position;
+        float step = speed * Time.deltaTime;
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+        transform.rotation = Quaternion.LookRotation(newDir);
+    **/
+    }
+}
